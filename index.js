9622a1185f2500afc3fb2ce987db3be18083254f
Reda El Khattabi <reda.ea@gmail.com>
2016-03-07 12:59:45 +0000

    main module: proxy class

---
+++ index.js
@@ -0,0 +1,79 @@
+
+var _ = require('lodash');
+
+var $filter = require('./filter');
+var $service = require('./service');
+var $client = require('./client');
+var $network = require('./network');
+
+module.exports = function() {
+    'use strict';
+
+    var self = this;
+    if(!self) self = {};
+
+    var client = new $client();
+
+    var _provide = function(service, type, filter) {
+        var specs = service;
+        if(typeof service == 'function')
+            specs = {
+                method: service,
+                filters: [{
+                    type: type,
+                    data: filter
+                }]
+            };
+        client.services.push(_.extend(new $service(_.bind(specs.method, self)), {
+            filters: specs.filters.map(function(f) {
+                return new $filter(f.type, f.data);
+            })
+        }));
+        return self;
+    };
+
+    var _connect = function(peers, port, callback) {
+        if(!_.isArray(peers))
+            peers = [peers];
+        if(typeof peers[0] != 'object') {
+            callback = port;
+            port = peers[0];
+            peers = [{ip: 'localhost', port: 9338}];
+        }
+        if(typeof port != 'number') {
+            callback = port;
+            port = 0;
+        }
+        if(typeof callback != 'function')
+            callback = _.noop;
+        client.network = new $network(client, peers, {port: port}, function(err) {
+            if(err)
+                return callback(err, null);
+            delete self.provide;
+            delete self.connect;
+            self.send = _send;
+            self.disconnect = _disconnect;
+            return callback(null, self);
+        });
+        return self;
+    };
+
+    var _send = function(message, cb) {
+        client.handle(message, cb);
+        return self;
+    };
+
+    var _disconnect = function() {
+        client.network.close();
+        client.network = null;
+        delete self.send;
+        delete self.disconnect;
+        self.provide = _provide;
+        self.connect = _connect;
+        return self;
+    };
+
+    self.provide = _provide;
+    self.connect = _connect;
+    return self;
+};
