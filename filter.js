9476913c007fb7aea35edb796119c96f0fc5934d
Reda El Khattabi <reda.ea@gmail.com>
2016-04-13 19:20:49 -0700

    JSON Schema filter support

--- filter.js
+++ filter.js
@@ -1,5 +1,6 @@
 
 var _ = require('lodash');
+var ajv = require('ajv')({v5: true});
 
 var Filter = function(type, data) {
     this.type = type;
@@ -9,12 +10,17 @@ var Filter = function(type, data) {
 Filter.prototype.check = function(message) {
     if(!Filter.types[this.type])
         return false;
-    return Filter.types[this.type](this.data, message)
+    return Filter.types[this.type].call(this, this.data, message);
 };
 
 Filter.types = {
     object: function(data, message) {
         return _.isMatch(message, data);
+    },
+    schema: function(data, message) {
+        if(!this._validator)
+            this._validator = ajv.compile(data);
+        return this._validator(message);
     }
 };
 
